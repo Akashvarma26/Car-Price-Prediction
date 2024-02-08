7f441d71421ad25bead5489892546c1c58d74fe7
Akash Varma Datla <akashvdatla@gmail.com>
2024-02-08 10:19:53 +0530

    commit-new

--- app.py
+++ app.py
@@ -14,10 +14,24 @@ def predict():
     if request.method == 'POST':
         Present_Price = float(request.form['price'])
         Kms_Driven = int(request.form['kms_driven'])
-        Fuel_Type = request.form['fuel']
-        Seller_Type = request.form['seller']
-        Transmission = request.form['transmission']
-        no_of_year=request.form['year']
+        Fuel_Type = request.form['fuel'].lower()
+        Seller_Type = request.form['seller'].lower()
+        Transmission = request.form['transmission'].lower()
+        no_of_year=int(request.form['year'])
+        if Fuel_Type=="petrol":
+            Fuel_Type=2
+        elif Fuel_Type=="diesel":
+            Fuel_Type=1
+        elif Fuel_Type=="cng":
+            Fuel_Type=0
+        if Seller_Type=="dealer":
+            Seller_Type=1
+        elif Seller_Type=="individual":
+            Seller_Type=0
+        if Transmission=="manual":
+            Transmission=1
+        elif Transmission=="automatic":
+            Transmission=0
         prediction = model.predict([[Present_Price, Kms_Driven, Fuel_Type, Seller_Type, Transmission,no_of_year]])
         output = round(prediction[0], 2)
         return render_template('index.html', output="The Predicted price of the car is rs {} Lakhs".format(output))
