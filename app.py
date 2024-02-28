ac528f2cecc3fc732bd3fe9e5d25a96b4eda924e
Akash Varma Datla <akashvdatla@gmail.com>
2024-02-28 15:31:12 +0530

    New Updates to frontend(HTML)

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ def predict():
         prediction = model.predict([[Present_Price, Kms_Driven, Fuel_Type, Seller_Type, Transmission,no_of_year]])
         output = round(prediction[0], 2)
         return render_template('index.html', output="The Predicted price of the car is rs {} Lakhs".format(output))
-#if __name__ == '__main__':
-#   app.run(debug=True)
+if __name__ == '__main__':
+   app.run(debug=True)
