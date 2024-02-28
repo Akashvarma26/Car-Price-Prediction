b72e9ba1f5a1394a8d27c1b538e92fe164fe3e6c
Akash Varma Datla <akashvdatla@gmail.com>
2024-02-28 14:50:08 +0530

    New drop down for inputs

--- app.py
+++ app.py
@@ -14,26 +14,12 @@ def predict():
     if request.method == 'POST':
         Present_Price = float(request.form['price'])
         Kms_Driven = int(request.form['kms_driven'])
-        Fuel_Type = request.form['fuel'].strip().lower()
-        Seller_Type = request.form['seller'].strip().lower()
-        Transmission = request.form['transmission'].strip().lower()
+        Fuel_Type = int(request.form['fuel'])
+        Seller_Type = int(request.form['seller'])
+        Transmission = int(request.form['transmission'])
         no_of_year=int(request.form['year'])
-        if Fuel_Type=="petrol":
-            Fuel_Type=2
-        elif Fuel_Type=="diesel":
-            Fuel_Type=1
-        elif Fuel_Type=="cng":
-            Fuel_Type=0
-        if Seller_Type=="dealer":
-            Seller_Type=1
-        elif Seller_Type=="individual":
-            Seller_Type=0
-        if Transmission=="manual":
-            Transmission=1
-        elif Transmission=="automatic":
-            Transmission=0
         prediction = model.predict([[Present_Price, Kms_Driven, Fuel_Type, Seller_Type, Transmission,no_of_year]])
         output = round(prediction[0], 2)
         return render_template('index.html', output="The Predicted price of the car is rs {} Lakhs".format(output))
-if __name__ == '__main__':
-   app.run(debug=True)
+#if __name__ == '__main__':
+#   app.run(debug=True)
